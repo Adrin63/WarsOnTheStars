71330f29999d841c4d211f03b0f05aace0336d8b
Adrin63 <57983225+Adrin63@users.noreply.github.com>
2019-12-18 11:33:43 +0100

    asdf

--- WarsOnTheStarsProyect/Background.cpp
+++ WarsOnTheStarsProyect/Background.cpp
@@ -1,25 +1,32 @@
-#include "Background.h";
+#include "Background.h"
+#include <iostream>
+#include "FAriasSimpleGraphics.h"
 
+//Datos externos
+extern char starSkin;
+extern Game game;
+
+//Constantes
 const int MAX_INIT_STARS = 10;
-int starX, starY;
 
-char BKGMap[MAP_HEIGHT][MAP_WIDTH];
+//Variables
+Star stars[MAX_INIT_STARS];
 
-void Background()
+//Código
+void Init_Stars()
 {
-	/*for (int i = 0; i < MAX_INIT_STARS; i++)
+	for (int i = 0; i < MAX_INIT_STARS; i++)
 	{
-		starX = rand() % MAP_WIDTH;
-		starY = rand() % MAP_HEIGHT;
+		stars[i].starPos.X = rand() % game.W;
+		stars[i].starPos.Y = rand() % game.H;
+		
+	}
+}
 
-	}*/
-
-	for (int i = 0; i < MAP_HEIGHT; i++)
+void Draw_Stars()
+{
+	for (int i = 0; i < 1; i++)
 	{
-		for (int j = 0; j < MAP_WIDTH; j++)
-		{
-			std::cout << "*";
-		}
-		std::cout << "\n";
+		FASG::WriteCharBuffer(100, 20, '*', FASG::EForeColor::LightWhite);
 	}
 }
--- WarsOnTheStarsProyect/Console.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-const int MAP_HEIGHT = 50, MAP_WIDTH = 200;
-//int W, H;
