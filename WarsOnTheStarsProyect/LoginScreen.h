8ea67f970bea558f8a88c9eac385fa7ad2bc3f96
Adrin63 <57983225+Adrin63@users.noreply.github.com>
2019-12-19 12:15:10 +0100

    Update LoginScreen.h

--- WarsOnTheStarsProyect/LoginScreen.h
+++ WarsOnTheStarsProyect/LoginScreen.h
@@ -1,4 +1,4 @@
 #pragma once
 void LogScreen();
 void LogoWS();
-void Stick(int weight_start, int weight_end, int height_start, int height_end);
+void Tool(int weight_start, int weight_end, int height_start, int height_end);
