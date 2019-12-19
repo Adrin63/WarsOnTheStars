eb1ada6ca846ac5459c00cc214ce33d05dfddce6
Adrin63 <57983225+Adrin63@users.noreply.github.com>
2019-12-19 12:14:26 +0100

    qw

--- WarsOnTheStarsProyect/LoginScreen.cpp
+++ WarsOnTheStarsProyect/LoginScreen.cpp
@@ -5,8 +5,40 @@
 
 extern Game game;
 
+/*
+void Tool(int weight_start, int weight_end, int height_start, int height_end)
+{
+	for (int w = weight_start; w < weight_end; w++)
+	{
+		for (int h = height_start; h < height_end; h++)
+		{
+			std::cout << "_\n";
+			
+		}
+		std::cout << "\n";
+	}
+}
+
+void LogoWS()
+{
+	
+
+
+
+
+	for (int w = 0; w < game.W; w++)
+	{
+		for (int h = 0; h < game.H; h++)
+		{
+		
+		}
+	}
+}
+*/
+
 void LogScreen()
 {	
+	LogoWS();
 	FASG::WriteStringBuffer(41,FASG::EAligned::CENTER,"Presione cualquier tecla para continuar", FASG::EForeColor::White);
 	FASG::WriteStringBuffer(46, FASG::EAligned::CENTER, "X para cerrar", FASG::EForeColor::White);
 
@@ -21,3 +53,38 @@ void LogScreen()
 	}	
 
 }
+
+/*void DrawMap()
+{
+	for (int fila = 0; fila < MAP_H; fila++)
+	{
+		int columnaConsola = 0;
+		for (int columna = XCam; columna < XCam + W; columna++)
+		{
+			if (Map[fila][columna] == 'X')
+				FASG::WritePixelBuffer(columnaConsola, fila, FASG::EBackColor::LightGreen);
+
+			columnaConsola++;
+		}
+	}
+}
+
+
+
+
+void CreatePlatform(int x, int y, int width)
+{
+	for (int columna = x; columna < x + width; columna++)
+	{
+		if (InMapRange(columna, y))
+			Map[y][columna] = 'X';
+	}
+}
+
+void CreateStepPlatform(int x, int y, int step, int numStairs, int gap, int width)
+{
+	for (int stair = 0; stair < numStairs; stair++)
+	{
+		CreatePlatform(x + (gap + width) * stair, y + (step * stair), width);
+	}
+}*/
--- WarsOnTheStarsProyect/LoginScreen.h
+++ WarsOnTheStarsProyect/LoginScreen.h
@@ -1,2 +1,4 @@
 #pragma once
 void LogScreen();
+void LogoWS();
+void Stick(int weight_start, int weight_end, int height_start, int height_end);
